1df4aaebf64041a629a7fbc5cebccd2910b36c1a
yangrongZ <zhuyangrong.yvr.cn>
2025-07-23 10:51:29 +0800

    feat: 完成最基本的A*寻路

--- AStar/Assets/制作AStar/Scripts/Grid.cs
+++ AStar/Assets/制作AStar/Scripts/Grid.cs
@@ -31,7 +31,7 @@ public class Grid : MonoBehaviour
             {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint);
+                grid[x, y] = new Node(walkable, worldPoint,x,y);
             }
         }
     }
@@ -47,6 +47,27 @@ public class Grid : MonoBehaviour
         int y = Mathf.RoundToInt((gridSizeY - 1) * percentageY);
         return grid[x, y];
     }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+        for (int x = -1; x <= 1 ; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+                
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                    neighbours.Add(grid[checkX, checkY]);
+            }
+        }
+        return neighbours;
+    }
+
+    public List<Node> path; 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
@@ -60,6 +81,9 @@ public class Grid : MonoBehaviour
                 {
                     Gizmos.color = Color.cyan;
                 }
+                if(path != null)
+                    if(path.Contains(node))
+                        Gizmos.color = Color.black;
                 Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
             }
         }
--- AStar/Assets/制作AStar/Scripts/Node.cs
+++ AStar/Assets/制作AStar/Scripts/Node.cs
@@ -6,10 +6,18 @@ public class Node
 {
     public bool walkable;
     public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
 
-    public Node(bool walkable, Vector3 worldPosition)
+    public int gCost;
+    public int hCost;
+    public Node parent;
+    public int fCost => gCost + hCost;
+    public Node(bool walkable, Vector3 worldPosition,int gridX, int gridY)
     {
         this.walkable = walkable;
         this.worldPosition = worldPosition;
+        this.gridX = gridX;
+        this.gridY = gridY;
     }
 }
